f8c4bd3063fca84c311629d882021214ff62768a
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-10-10 13:32:13 +0700

    Update task 2.kt

--- task 2.kt
+++ task 2.kt
@@ -7,28 +7,22 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var main_btn: Button? = null
-    private var edit_text: EditText? = null
-    private var check_box: CheckBox? = null
-    private var text_viev: TextView? = null
-    private var progres: ProgressBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_btn = findViewById(R.id.button)
-        edit_text = findViewById(R.id.editTextTextPersonName)
-        check_box = findViewById(R.id.checkBox)
-        text_viev = findViewById(R.id.textView)
-        progres = findViewById(R.id.progressBar)
+        val button: Button = findViewById(R.id.button)
+        val editText: EditText = findViewById(R.id.editTextTextPersonName)
+        val checkBox:CheckBox = findViewById(R.id.checkBox)
+        val textViev:TextView = findViewById(R.id.textView)
+        val bar:ProgressBar = findViewById(R.id.progressBar)
 
-        main_btn?.setOnClickListener {
-            if (check_box?.isChecked == true) {
-                text_viev?.text = edit_text?.text
-                progres?.incrementProgressBy(10)
+        button.setOnClickListener {
+            if (checkBox.isChecked) {
+                textViev.text = editText.text
+                bar.progress += 10
 
             }
 
